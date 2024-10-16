6153a7625246c5f0e7c59a89cb79b7ad7c117ace
Zülal <zllnbn1997@gmail.com>
2024-10-16 12:19:41 +0300

    design new voting creating form

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,9 +1,44 @@
-function New() {
+import { useState } from "react";
+
+const initialOptions = [{ title: "" }, { title: "" }];
+
+function NewQuestion() {
+  const [title, setTitle] = useState("");
+  const [options, setOptions] = useState(initialOptions);
+
+  const handleChangeOption = ({ target }) => {
+    const newArray = options;
+    newArray[target.id].title = target.value;
+    setOptions([...newArray]);
+  };
+
   return (
     <div>
-      <h1>New Question</h1>
+      <h2>Question</h2>
+      <input
+        placeholder="Type your question"
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+      />
+      <h2>Options</h2>
+      {options.map((option, index) => (
+        <div key={index}>
+          <input
+            id={index}
+            placeholder={`Option ${index + 1}`}
+            value={option.title}
+            onChange={handleChangeOption}
+          />
+        </div>
+      ))}
+
+      <button onClick={() => setOptions([...options, { title: "" }])}>
+        New option
+      </button>
+
+      <button>Save</button>
     </div>
   );
 }
 
-export default New;
+export default NewQuestion;
